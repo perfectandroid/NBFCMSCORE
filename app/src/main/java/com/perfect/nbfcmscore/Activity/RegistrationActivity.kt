0f9fba17c3a5a1e65db3050cc3741471b5e54cb1
dilsha <psstechteam@gmail.com>
2021-09-30 11:39:26 +0530

    TestLog

--- app/src/main/java/com/perfect/nbfcmscore/Activity/RegistrationActivity.kt
+++ app/src/main/java/com/perfect/nbfcmscore/Activity/RegistrationActivity.kt
@@ -4,6 +4,7 @@ import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -33,6 +34,8 @@ class RegistrationActivity : AppCompatActivity()  , View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         setRegViews()
+
+        Log.i("commit","Test")
     }
 
     private fun setRegViews() {
