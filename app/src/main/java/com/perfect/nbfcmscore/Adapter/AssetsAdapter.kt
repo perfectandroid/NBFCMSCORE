c55ea4e17e10ff3c3ca64ec33998d653b85a63ac
dilsha <psstechteam@gmail.com>
2021-11-01 17:37:34 +0530

    Dashboard data added

---
+++ app/src/main/java/com/perfect/nbfcmscore/Adapter/AssetsAdapter.kt
@@ -0,0 +1,72 @@
+package com.perfect.nbfcmscore.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.perfect.nbfcmscore.Helper.ItemClickListener
+import com.perfect.nbfcmscore.R
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+class AssetsAdapter (internal val mContext: Context, internal val jsInfo: JSONArray): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    internal var jsonObject: JSONObject? = null
+    private var clickListener: ItemClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): RecyclerView.ViewHolder {
+        val vh: RecyclerView.ViewHolder
+        val v = LayoutInflater.from(parent.context).inflate(
+            R.layout.adapter_accountlist, parent, false
+        )
+        vh = MainViewHolder(v)
+        return vh
+    }
+
+    override fun getItemCount(): Int {
+        return jsInfo.length()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        try {
+
+            jsonObject = jsInfo.getJSONObject(position)
+
+            if (holder is AssetsAdapter.MainViewHolder) {
+                holder.tv_adp_account!!.setText(jsonObject!!.getString("AccountNumber"))
+
+                holder.ll_adp_account!!.setOnClickListener(View.OnClickListener {
+                    clickListener!!.onClick(position,"account")
+
+                })
+
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    inner class MainViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+
+        internal var ll_adp_account: LinearLayout? = null
+        var tv_adp_account: TextView? = null
+
+
+        init {
+
+            ll_adp_account = v.findViewById<View>(R.id.ll_adp_account) as LinearLayout
+            tv_adp_account = v.findViewById<View>(R.id.tv_adp_account) as TextView
+
+
+        }
+    }
+
+    fun setClickListener(itemClickListener: ItemClickListener?) {
+        clickListener = itemClickListener
+    }
+}
