9ab744777a4c94b8ab8215671571e0b0fae4ca3c
dilsha <psstechteam@gmail.com>
2021-11-13 12:53:06 +0530

    Executive call back with send mail

--- app/src/main/java/com/perfect/nbfcmscore/Activity/ExecutiveActivity.kt
+++ app/src/main/java/com/perfect/nbfcmscore/Activity/ExecutiveActivity.kt
@@ -1,25 +1,79 @@
 package com.perfect.nbfcmscore.Activity
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.perfect.nbfcmscore.Helper.Config
-import com.perfect.nbfcmscore.Helper.PicassoTrustAll
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import com.perfect.nbfcmscore.R
 
-class ExecutiveActivity : AppCompatActivity() {
+class ExecutiveActivity : AppCompatActivity() ,View.OnClickListener{
 
     var imgBack: ImageView? = null
-    var applogo: ImageView? = null
-    var imCompanylogo: ImageView? = null
     var imgHome: ImageView? = null
+    var txtv_Name: TextInputEditText? = null
+    var txtv_mob: TextInputEditText? = null
+    var txtv_Date: TextInputEditText? = null
+    var txt_time: TextInputEditText? = null
+    var btn_submit: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_executive)
 
+        setRegViews()
+    }
 
+    private fun setRegViews() {
+        imgBack = findViewById<ImageView>(R.id.imgBack)
+        imgBack!!.setOnClickListener(this)
+        imgHome = findViewById<ImageView>(R.id.imgHome)
+        imgHome!!.setOnClickListener(this)
+
+        txtv_Name= findViewById<TextInputEditText>(R.id.txtv_Name)
+        txtv_mob= findViewById<TextInputEditText>(R.id.txtv_mob)
+        txtv_Date= findViewById<TextInputEditText>(R.id.txtv_Date)
+        txt_time= findViewById<TextInputEditText>(R.id.txt_time)
+
+        btn_submit= findViewById<Button>(R.id.btn_submit)
+
+
+        btn_submit!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.imgBack -> {
+                finish()
+            }
+            R.id.imgHome -> {
+                startActivity(Intent(this@ExecutiveActivity, HomeActivity::class.java))
+            }
+            R.id.btn_submit -> {
+                sendEmail()
+            }
+        }
+    }
+
+    private fun sendEmail() {
+        //val Sendemail = applicationContext.getSharedPreferences("Sendemail", 0)
+        //val Sendmail = Sendemail.getString("Sendemail", "")
+      //  Toast.makeText(this@SuggestionActivity, Sendmail + "", Toast.LENGTH_SHORT).show()
+        val TO = arrayOf("psstechteam@gmail.com")
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        emailIntent.data = Uri.parse("mailto:")
+        emailIntent.type = "text/plain"
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO)
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Call back Form")
+        var data =txtv_Name!!.text.toString()+"\n"+txtv_mob!!.text.toString()+"\n"+
+                txtv_Date!!.text.toString()+"\n"+txt_time!!.text.toString()
+        emailIntent.putExtra(Intent.EXTRA_TEXT, data)
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."))
+        } catch (ex: ActivityNotFoundException) {
+        }
     }
 }
