3d51e98ac278165771aaf273c5c4a490915e391d
nihal <nihalpavithran1@gmail.com>
2023-07-11 18:17:54 +0530

    Layout issue with large font and zoom view fixed

--- app/src/main/java/com/perfect/nbfcmscore/Activity/OwnBankownaccountFundTransfer.kt
+++ app/src/main/java/com/perfect/nbfcmscore/Activity/OwnBankownaccountFundTransfer.kt
@@ -341,21 +341,21 @@ class OwnBankownaccountFundTransfer : AppCompatActivity(), View.OnClickListener,
                             applicationContext.getSharedPreferences(Config.SHARED_PREF313, 0)
                         val BankHeaderPref = BankHeaderSP.getString("BankHeader", null)
 
-                        requestObject1.put("Reqmode","26")
-                        requestObject1.put("Token",Token)
+                        requestObject1.put("Reqmode",MscoreApplication.encryptStart("26"))
+                        requestObject1.put("Token",MscoreApplication.encryptStart(Token))
                         requestObject1.put(
                             "FK_Customer",
-                            FK_Customer
-                        )
+                            MscoreApplication.encryptStart(FK_Customer
+                        ))
                         requestObject1.put(
                             "SubMode",
-                           "2"
-                        )
-                        requestObject1.put("BankKey", BankKeyPref)
+                            MscoreApplication.encryptStart("2"
+                        ))
+                        requestObject1.put("BankKey", MscoreApplication.encryptStart(BankKeyPref))
                         requestObject1.put(
                             "BankHeader",
-                            BankHeaderPref
-                        )
+                            MscoreApplication.encryptStart(BankHeaderPref
+                        ))
 
 
                         Log.e("TAG", "requestObject1  171   " + requestObject1)
